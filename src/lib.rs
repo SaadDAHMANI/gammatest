1463391857025cfceb5937cabdb15b218565e12b
SaadDAHMANI <tahatatakor7@gmail.com>
2022-08-14 13:08:33 +0100

    add documentation + example

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,65 @@
-//
-// Gamma Test, based on ()  
-//
+//!
+//! [gammatest](https://github.com/SaadDAHMANI/gammatest) is a simple implementation of the Gamma Test.
+//! Gamma Test [1] is non-parametric test for feature selection frequently used in machine learning.
+//! The [gammatest](https://github.com/SaadDAHMANI/gammatest) crate is base on https://ijssst.info/Vol-06/No-1&2/Kemp.pdf[2] paper.
+//! 
+//! ## References 
+//! [1] Stefánsson, A., Končar, N., & Jones, A. J. (1997). A note on the gamma test. Neural Computing & Applications, 5(3), 131-133.
+//! [2] Kemp, S. E., Wilson, I. D., & Ware, J. A. (2004). A tutorial on the gamma test. International Journal of Simulation: Systems, Science and Technology, 6(1-2), 67-75.
+
 use num_traits::Float;
 
 ///
 /// GammaTest struct computes Gamma Test for a given (inputs/outputs) dataset in lazy way.
+///
+/// # Example :
+/// ```
+/// use gammatest::*;
+/// fn main()
+///    {    
+///       // Give the input matrix
+///       let inputs =[
+///                   [3.0f32, 4.0, 4.0].to_vec(),
+///                   [2.0f32, 1.0, 3.0].to_vec(),
+///                   [1.0f32, 0.0, 1.0].to_vec(),
+///                   [1.0f32, 1.0, 1.0].to_vec(),
+///                ];
 /// 
+///       // Give the output vector 
+///       let output = [54.0f32, 30.0, 3.0, 28.0];
+///
+///       // p is the number of neighbors 
+///       let p : usize = 3;
+///      
+///       // Build the GammaTest using f32 data type
+///       let mut gt : GammaTest<f32> = GammaTest::new(&inputs, &output, p);
+///   
+///       // To use f64 data type 
+///       //let mut gt : GammaTest<f64> = GammaTest::new(&inputs, &output, p);
+///
+///       // Call function compute() to compute GammaTest parameters.
+///       gt.compute();
+///
+///       // Check results
+///       assert_eq!(gt.slope,  Some(33.54095));
+///       assert_eq!(gt.intercept, Some(20.578278));
+///     } 
+///  ``` 
+
 pub struct GammaTest<'a, T : Float> {
-      
+    
+    /// The Gamma parameter (intercept)   
     pub intercept : Option<T>,
+
+    /// The slope value
     pub slope : Option<T>,
+
+    /// The V_ratio value 
     pub v_ratio : Option<T>,
+ 
+    /// The number of neighbors
     pub p : usize,
+    
     pub output_variance : Option<T>,
     //pub mean_absolute_error : Option<f32>,
     pub sigmas : Option<Vec<T>>,
@@ -74,27 +122,6 @@ impl<'a, T:Float> GammaTest<'a, T> {
    
     ///
     /// Call this function to compute Gamma Test values (Slope, Intercep and V_ratio).
-    /// 
-    /// Example:
-    /// ```
-    /// use gammatest::*;
-    /// 
-    /// fn main()
-    /// {
-    ///      let output = [54.0f32, 30.0, 3.0, 28.0];
-    ///      let inputs =[
-    ///                  [3.0f32, 4.0, 4.0].to_vec(),
-    ///                  [2.0f32, 1.0, 3.0].to_vec(),
-    ///                  [1.0f32, 0.0, 1.0].to_vec(),
-    ///                  [1.0f32, 1.0, 1.0].to_vec(),
-    ///                 ];
-    ///      let p : usize = 3;
-    ///      let mut gt : GammaTest<f32> = GammaTest::new(&inputs, &output, p);
-    ///      gt.compute();
-    ///      assert_eq!(gt.slope,  Some(33.54095));
-    ///      assert_eq!(gt.intercept, Some(20.578278));
-    /// } 
-    /// ``` 
     ///  
     pub fn compute(&mut self){
             
